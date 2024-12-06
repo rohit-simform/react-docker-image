8bd81e01b6c155fdeee567c5c187b0cd015442c9
rohit-simform <rohit.s@simformsolutions.com>
2024-12-06 12:01:40 +0530

    update code to test CICD functionality

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi This is Rohit Sharam</h1>
+      <p>Updated code via CI/CD</p>
     </div>
   );
 }

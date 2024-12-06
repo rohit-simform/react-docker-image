42f2c86e9d9c79f7dca7696161e408e7555d6975
rohit-simform <rohit.s@simformsolutions.com>
2024-12-06 12:43:53 +0530

    content change

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hi This is Rohit Sharam</h1>
+      <h1>Hi This is Rohit Sharma</h1>
       <p>Updated code via CI/CD</p>
+      <hr/>
+      <p>You can also create and run the image of this React App as there is configured Docker</p>
     </div>
   );
 }

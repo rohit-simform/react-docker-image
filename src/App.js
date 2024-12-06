8993c7e80a19d3686898222bf8ba49627b663418
rohit-simform <rohit.s@simformsolutions.com>
2024-12-06 21:43:50 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <h1>Hi This is Rohit Sharma</h1>
       <p>Updated code via CI/CD</p>
       <hr/>
-      <p>You can also create and run the image of this React App as there is configured Docker</p>
+      <p>This App is hosted on S3 bucket and EC2(via Docker)</p>
     </div>
   );
 }
